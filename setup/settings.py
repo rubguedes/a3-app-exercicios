5f739f186e3962eda1ce45671ca5299b148954f9
Ruben Guedes <96798858+rubguedes@users.noreply.github.com>
2024-06-10 14:56:25 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%*ps2$lv)^#!!p(yiu+uslo3b-!p4#0j@k#-esrtz@8x6c$^+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
