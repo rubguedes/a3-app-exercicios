308fe8591894c69f647cfa2c1b19e6b28ee66207
Ruben Guedes <96798858+rubguedes@users.noreply.github.com>
2024-06-10 14:55:25 -0300

    Update serializer.py

--- usuario/serializer.py
+++ usuario/serializer.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from usuario.models import Usuario
+from usuario.models import Usuario, Historico
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['id', 'nome_completo', 'cpf', 'data_nascimento', 'telefone', 'email', 'historico']
+        exclude = []
+
+class HistoricoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Historico
+        exclude = []
+
+class HistoricoExercicioUsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Historico
+        exclude = []
+
+class UsuarioSerializerv2(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        exclude = ['senha']
+
